e59118967bbf8879250c2fe752a738066cab672d
Vania 102383 <vania.morais@ua.pt>
2024-10-23 17:14:45 +0100

    Criar pasta out

--- mp1-contentfull-tdw/next.config.js
+++ mp1-contentfull-tdw/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
+    unoptimized: true,
     loader: "custom",
     formats: ["image/avif", "image/webp"],
   },
   experimental: {
     forceSwcTransforms: true, //isto obriga o SWC a ser o compilador, ao invés do babel
   },
+  output: "export",
 };
